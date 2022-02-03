8d8cf99f3400686f41327f12d7b7633b336e602d
tristan <tristan.sukhnandan@gmail.com>
2022-02-03 21:34:15 +0200

    Added in additional logic to Login Page Object class

--- src/main/java/pageObjects/Login_PO.java
+++ src/main/java/pageObjects/Login_PO.java
@@ -2,6 +2,8 @@ package pageObjects;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import utils.Global_Vars;
 
 public class Login_PO extends Base_PO{
     private @FindBy(id = "text")
@@ -13,12 +15,14 @@ public class Login_PO extends Base_PO{
     private @FindBy(id = "login-button")
     WebElement login_Button;
 
+    //private @FindBy(how = How.XPATH, using = "")
+
     public Login_PO(){
         super();
     }
 
     public void navigateTo_WebDriverUniversity_Login_Page(){
-        navigateTo_URL("https://www.webdriveruniversity.com/Login-Portal/index.html?");
+        navigateTo_URL(Global_Vars.WEBDRIVER_UNI_HOMEPAGE_URL + "/Login-Portal/index.html?");
     }
 
     public void setUsername(String username){
@@ -31,5 +35,13 @@ public class Login_PO extends Base_PO{
 
     public void clickLoginButton(){
         waitForWebElementAndClick(login_Button);
+    }
+
+    public void validate_SuccessfulLogin_Message(){
+        waitForAlert_And_ValidateText("validation succeeded");
+    }
+
+    public void validate_UnsuccessfulLogin_Message(){
+        waitForAlert_And_ValidateText("validation failed");
     }
 }
--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"src/test/resources/features"}, glue = {"stepDefinitions"},
-                tags = "@First", monochrome = true, dryRun = false,
+                tags = "@Login", monochrome = true, dryRun = false,
                 plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 
 public class MainRunner extends AbstractTestNGCucumberTests {
